5f616a2e4ebd2d92767c2478529280fdfa9ec70e
lianstemp <mrxazk@protonmail.com>
2025-02-14 03:57:29 +0700

    Update API endpoints to use new domain and add CloudFront distribution for frontend

--- infra/main.tf
+++ infra/main.tf
@@ -45,6 +45,14 @@ resource "aws_security_group" "backend" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -125,15 +133,13 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
+        Sid       = "AllowCloudFrontAccess"
         Effect    = "Allow"
-        Principal = "*"
-        Action    = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.frontend.arn}/*"
-        ]
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.frontend.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
       }
     ]
   })
@@ -151,4 +157,64 @@ resource "aws_s3_account_public_access_block" "account" {
 resource "aws_cloudwatch_log_group" "api_gw_logs" {
   name              = "/aws/apigateway/quantum_slot_api"
   retention_in_days = 14
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled             = true
+  default_root_object = "index.html"
+  
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+    
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id      = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+    
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  # Cheapest option that includes Taiwan
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "frontend" {
+  comment = "OAI for quantum casino frontend"
+}
+
+# Output the CloudFront domain name
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.frontend.domain_name
 }
